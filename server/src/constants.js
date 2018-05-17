bb2802abb7f00cc2e52b2c0dc0fa5645b3d8f5e7
Kyle Geib <kyle.geib@gmail.com>
2018-05-16 22:05:05 -0700

    ISSUE-006 Server: Answer syntax first draft. Random improvements to question syntax. Changed survey question and answer design, including dropping the 'rounding' option. Defined recognized metric and imperial units - I needed a method to decide if different symbols were recognized as units and realized I would need to name them in the server when generating question + answer objects.

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,4 +1,6 @@
 const constants = {
+
+  // Database Constants
   // Generic
   FLAGS_NONE: 0,
 
@@ -35,6 +37,171 @@ const constants = {
   QUESTION_DIFFICULTY_MEDIUM: 3,
   QUESTION_DIFFICULTY_MEDIUM_HARD: 4,
   QUESTION_DIFFICULTY_HARD: 5,
+
+  // Answer
+  ANSWER_TYPE_MULTIPLE_CHOICE: 0,
+  ANSWER_TYPE_CONVERSION: 1,
+
+  // Data constants
+  // Units to English
+  UNITS: {
+    // Length - Metric
+    mm: {
+      singular: "millimeter",
+      plural: "millimeters",
+    },
+    cm: {
+      singular: "centimeter",
+      plural: "centimeters",
+    },
+    m: {
+      singular: "meter",
+      plural: "meters",
+    },
+    km: {
+      singular: "kilometer",
+      plural: "kilometers",
+    },
+    // Length - Imperial
+    in: {
+      singular: "inch",
+      plural: "inches",
+    },
+    ft: {
+      singular: "foot",
+      plural: "feet",
+    },
+    yd: {
+      singular: "yard",
+      plural: "yards",
+    },
+    mi: {
+      singular: "mile",
+      plural: "miles",
+    },
+    // Mass - Metric
+    mg: {
+      singular: "milligram",
+      plural: "milligrams",
+    },
+    g: {
+      singular: "gram",
+      plural: "grams",
+    },
+    kg: {
+      singular: "kilogram",
+      plural: "kilograms",
+    },
+    tonne: {
+      singular: "tonne",
+      plural: "tonnes",
+    },
+    // Mass - Imperial
+    oz: {
+      singular: "ounce",
+      plural: "ounces",
+    },
+    lb: {
+      singular: "pound",
+      plural: "pounds",
+    },
+    ton: {
+      singular: "short ton",
+      plural: "short tons",
+    },
+    lton: {
+      singular: "long ton",
+      plural: "long tons",
+    },
+    st: {
+      singular: "stone",
+      plural: "stone",
+    },
+    // Volume - Metric
+    ml: {
+      singular: "milliliter",
+      plural: "milliliters",
+    },
+    l: {
+      singular: "liter",
+      plural: "liters",
+    },
+    // Volume - Imperial
+    cuin: {
+      singular: "cubic inch",
+      plural: "cubic inches",
+    },
+    floz: {
+      singular: "fluid ounce",
+      plural: "fluid ounces",
+    },
+    cup: {
+      singular: "cup",
+      plural: "cups",
+    },
+    pint: {
+      singular: "pint",
+      plural: "pints",
+    },
+    gal: {
+      singular: "gallon",
+      plural: "gallons",
+    },
+    // Temperature - Metric
+    c: {
+      singular: "Celsius",
+      plural: "Celsius",
+    },
+    // Temperature - Imperial
+    f: {
+      singular: "Fahrenheit",
+      plural: "Fahrenheit",
+    },
+    // Area - Metric
+    sqm: {
+      singular: "square meter",
+      plural: "square meters",
+    },
+    ha: {
+      singular: "hectare",
+      plural: "hectares",
+    },
+    sqkm: {
+      singular: "square kilometer",
+      plural: "square kilometers",
+    },
+    // Area - Imperial
+    sqft: {
+      singular: "square foot",
+      plural: "square feet",
+    },
+    acre: {
+      singular: "acre",
+      plural: "acres",
+    },
+    sqmi: {
+      singular: "square mile",
+      plural: "square miles",
+    },
+    // Velocity - Metric
+    ms: {
+      singular: "meter per second",
+      plural: "meters per second",
+    },
+    kmph: {
+      singular: "kilometer per hour",
+      plural: "kilometers per hour",
+    },
+    // Velocity - Imperial
+    fps: {
+      singular: "foot per second",
+      plural: "feet per second",
+    },
+    mph: {
+      singular: "mile per hour",
+      plural: "miles per hour",
+    },
+  },
 
 };
 
--- server/src/qaSyntax.js
+++ server/src/qaSyntax.js
@@ -6,17 +6,20 @@ const {
   QUESTION_TYPE_WRITTEN,
   QUESTION_TYPE_CONVERSION,
   QUESTION_TYPE_SURVEY,
+  ANSWER_TYPE_MULTIPLE_CHOICE,
+  ANSWER_TYPE_CONVERSION,
+  UNITS,
 } = require("./constants");
 
 function parseQAStrings(type, question, answer) {
   const questionPayload = this.parseQuestionString(type, question);
-  const answerPayload = this.parseAnswerString(answer, questionPayload);
+  const answerPayload = this.parseAnswerString(answer);
 }
 
 function parseQuestionString(type, question) {
-  const basePattern = /\[([^\]]+)]/;        // Finds "[20.5-30c(0.5)s]"; Returns "20-30c(0.5)s"
-  const rangePattern = /[\d.]+-[\d.]+\w+/;  // Finds "20.5-30c"; Returns "20.5", "30", "c"
-  const stepPattern = /\(([\d.]+)\)s/;      // Finds "(0.5)s"; Returns "0.5"
+  const basePattern = /\[([^\]]+)]/;              // Finds "[20.5-30c(0.5)s]"; Returns "20-30c(0.5)s"
+  const rangePattern = /([\d.]+)-([\d.]+)(\w+)/;  // Finds "20.5-30c"; Returns "20.5", "30", "c"
+  const stepPattern = /\(([\d.]+)\)s/;            // Finds "(0.5)s"; Returns "0.5"
 
   const baseResult = question.match(basePattern);
 
@@ -36,19 +39,26 @@ function parseQuestionString(type, question) {
     const rangeResult = baseResult[0].match(rangePattern);
 
     // Make sure we have the expected number of results for range minimum, range maximum, and unit.
-    if (rangeResult.length !== 4) {
+    if (!rangeResult[1] || !rangeResult[2] || !rangeResult[3]) {
       throw new QuestionSyntaxError(question, "Range syntax is invalid");
     }
 
     // Parse the range.
     const valueA = parseFloat(rangeResult[1]);
     const valueB = parseFloat(rangeResult[2]);
-    const unit = rangeResult[3];
+    const unit = rangeResult[3].toLowerCase();
+
+    // Make sure the numbers were readable and in the correct order.
     if (Number.isNaN(valueA) || Number.isNaN(valueB)) {
       throw new QuestionSyntaxError(question, "Range contains invalid number(s)");
     }
     if (valueA > valueB) {
       throw new QuestionSyntaxError(question, "Range needs to be smaller-larger");
+    }
+
+    // Make sure the unit is recognized.
+    if (UNITS[unit] === undefined) {
+      throw new QuestionSyntaxError(question, `Unit "${unit}" not recognized`);
     }
 
     questionPayload.data.rangeBottom = valueA;
@@ -60,7 +70,7 @@ function parseQuestionString(type, question) {
     if (stepResult !== null) {
       const stepValue = parseFloat(stepResult[1]);
       if (Number.isNaN(stepValue)) {
-        throw new QuestionSyntaxError(question, "Step contains invalid number");
+        throw new QuestionSyntaxError(question, "Step value contains invalid number");
       }
       questionPayload.data.step = stepValue;
     } else {
@@ -90,6 +100,97 @@ function parseQuestionString(type, question) {
   }
 }
 
-function parseAnswerString(answer, questionPayload) {
-  //
+function parseAnswerString(answer) {
+  const basePattern = /\[([^\]]+)](\d{0,2})/;   // Finds "[m(0.5)a]"; Returns "m(0.5)a"
+  const multipleChoiceDelimiter = "|";          // Splits on |
+  const answerPattern = /([\d.]+)(\w+)/;        // Finds "2.5m"; Returns "2.5", "m"
+  const unitPattern = /%(\w+)/;                 // Finds "m"; Returns "m"
+  const unitAccuracyPattern = /\(([\d.]+)\)a/;  // Finds "(0.5)a"; Returns "0.5"
+
+  // Create the basic return payload now.
+  const answerPayload = {
+    type: null,
+    data: {
+    },
+  };
+
+  // Perform the base match.
+  const baseResult = answer.match(basePattern);
+  if (baseResult === null) {
+    throw new AnswerSyntaxError(answer, "Invalid answer syntax");
+  }
+
+  // Multiple choice answer...
+  if (baseResult[1].indexOf(multipleChoiceDelimiter) !== -1) {
+    // Split the choices
+    const multipleChoiceAnswers = baseResult[1].split(multipleChoiceDelimiter);
+
+    // Parse the choices offered.
+    const choicesOffered = baseResult[2] ?
+      Number.parseInt(baseResult[2], 10) : multipleChoiceAnswers.length;
+    if (Number.isNaN(choicesOffered) || choicesOffered <= 0) {
+      throw new AnswerSyntaxError(answer, "Invalid choice count defined");
+    }
+
+    // Parse the choices for values and units.
+    const parsedMultipleChoiceAnswers = multipleChoiceAnswers.map((singleAnswer) => {
+      const answerResult = singleAnswer.match(answerPattern);
+      if (answerResult === null) {
+        throw new AnswerSyntaxError(answer, `Answer choice "${singleAnswer}" not valid`);
+      }
+
+      // Parse the value.
+      const value = Number.parseFloat(answerResult[1]);
+      const unit = answerResult[2];
+
+      // Make sure the number was readable.
+      if (Number.isNaN(value)) {
+        throw new AnswerSyntaxError(answer,
+          `Answer choice "${singleAnswer}" contains invalid number`);
+      }
+
+      // Make sure the unit is recognized.
+      if (UNITS[unit] === undefined) {
+        throw new AnswerSyntaxError(answer, `Answer choice "${singleAnswer}" unit not recognized`);
+      }
+
+      return {
+        value,
+        unit,
+      };
+    });
+
+    answerPayload.type = ANSWER_TYPE_MULTIPLE_CHOICE;
+    answerPayload.data.choices = parsedMultipleChoiceAnswers;
+
+    return answerPayload;
+
+  // Conversion answer...
+  } else {
+    // Parse the unit.
+    const unitResult = baseResult[1].match(unitPattern);
+    if (unitPattern !== null) {
+      const unit = unitResult[1];
+      if (UNITS[unit] === undefined) {
+        throw new AnswerSyntaxError(answer, `Unit "${unit}" not recognized`);
+      }
+      answerPayload.data.unit = unit;
+    }
+
+    // Parse the accuracy.
+    const accuracyResult = baseResult[1].match(unitAccuracyPattern);
+    if (accuracyResult !== null) {
+      const accuracyValue = parseFloat(accuracyResult[1]);
+      if (Number.isNaN(accuracyValue)) {
+        throw new AnswerSyntaxError(answer, "Accuracy value contains invalid number");
+      }
+      answerPayload.data.accuracy = accuracyValue;
+    } else {
+      answerPayload.data.accuracy = 1.0;  // Defaults to 1.0
+    }
+
+    answerPayload.type = ANSWER_TYPE_CONVERSION;
+
+    return answerPayload;
+  }
 }
