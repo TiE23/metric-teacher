2585d6814fc9627f0639479c4fe202c79a8d3f66
Kyle Geib <kyle.geib@gmail.com>
2018-06-03 18:52:47 -0700

    ISSUE-026 Server: Add doc comments to logic class functions.

--- server/src/logic/qaGenerator.js
+++ server/src/logic/qaGenerator.js
@@ -21,6 +21,20 @@ const {
 } = require("./qaSyntax");
 
 
+/**
+ * Main function takes in question data from the database and, optionally, a matching student
+ * survey row of data. Using smaller functions returns a large constructed object containing
+ * data that is intended for consumption by the client.
+ * @param questionData
+ * @param surveyData
+ * @returns {{
+ *  questionId: *,
+ *  subSubjectId: *,
+ *  difficulty: *
+ *  question: {type, text, detail, data},
+ *  answer: {type: *, data: {detail: *}}
+ * }}
+ */
 function qaGenerate(questionData, surveyData = null) {
   const { questionPayload, answerPayload } = parseQAStrings(
     questionData.type,
@@ -50,6 +64,13 @@ function qaGenerate(questionData, surveyData = null) {
 }
 
 
+/**
+ * This function takes care of generating the content of the question object inside the QA object.
+ * @param questionPayload
+ * @param answerUnit
+ * @param surveyData
+ * @returns {*}
+ */
 function generateQuestionData(questionPayload, answerUnit = null, surveyData = null) {
   // Written question
   if (questionPayload.type === QUESTION_TYPE_WRITTEN) {
@@ -137,6 +158,16 @@ function generateQuestionData(questionPayload, answerUnit = null, surveyData = n
 }
 
 
+/**
+ * This function takes care of generating the content of the answer object inside the QA object.
+ * It's mostly just organizing of other more important functions. If the question is a written
+ * question, a conversion question, or a survey question will determine how the returned data
+ * looks.
+ * @param questionPayload
+ * @param answerPayload
+ * @param generatedQuestion
+ * @returns {{type: *, data: {detail: *}}}
+ */
 function generateAnswerData(questionPayload, answerPayload, generatedQuestion) {
   const generatedAnswer = {
     type: answerPayload.type,
@@ -201,6 +232,12 @@ function generateAnswerData(questionPayload, answerPayload, generatedQuestion) {
 }
 
 
+/**
+ * Simple function essentially just returns the answerPayload. I just wanted to give it its own
+ * function to follow the pattern.
+ * @param answerPayload
+ * @returns {{choicesOffered: *, choices: *|*[]|string[]}}
+ */
 function composeWrittenAnswerData(answerPayload) {
   return {
     choicesOffered: answerPayload.data.choicesOffered,
@@ -209,6 +246,25 @@ function composeWrittenAnswerData(answerPayload) {
 }
 
 
+/**
+ * This function takes care of generating the extact, rounded, friendly conversions values to the
+ * question. Additionally, it calls makeChoices() to return the multiple choice answers as well.
+ * @param fromValue
+ * @param fromUnit
+ * @param toUnit
+ * @param toAccuracy
+ * @returns {{
+ *  accuracy: *,
+ *  exact: *,
+ *  rounded:   *,
+ *  friendly: *,
+ *  range: {
+ *    bottom: {value, unit: *},
+ *    top: {value, unit: *}
+ *  },
+ *  choices: *
+ * }}
+ */
 function composeConversionAnswerData(fromValue, fromUnit, toUnit, toAccuracy) {
   const {
     exactValue,
@@ -248,6 +304,13 @@ function composeConversionAnswerData(fromValue, fromUnit, toUnit, toAccuracy) {
 }
 
 
+/**
+ * Simple function that generates survey review choices.
+ * @param value
+ * @param unit
+ * @param step
+ * @returns {{choices: *}}
+ */
 function composeSurveyAnswerData(value, unit, step) {
   const roundingLevel = UNITS[unit].round;
   const isTemperature = UNITS[unit].subject === "temperature";
@@ -283,6 +346,13 @@ function makeChoices(value, roundingLevel, unit, step, isTemperature) {
 }
 
 
+/**
+ * Simple function randomly selects a value from a bottom value, a top value, and a step.
+ * @param bottom
+ * @param top
+ * @param step
+ * @returns {*}
+ */
 function makeValueFromRange(bottom, top, step) {
   if (step <= 0 || bottom === top) {
     return bottom;
