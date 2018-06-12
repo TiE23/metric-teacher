229919792ef8ba5ac8f03462c2e096c35c8f1039
Kyle Geib <kyle.geib@gmail.com>
2018-06-12 12:12:47 -0700

    ISSUE-013 Server: Classroom queries: classroom, classrooms, and classroomSearch.

--- server/src/resolvers/Query/classroom.js
+++ server/src/resolvers/Query/classroom.js
@@ -1,11 +1,103 @@
-/*
-  TODO Query/classroom.js notes
-  Make it so that querying classrooms only gets students whose course is currently active.
-  Basically, if a student switches courses, they will no longer exist.
-  Additionally, perhaps create an alternative query where it DOES show these inactive course
-  students so that if there is a case where a teacher is wondering why one of their students
-  suddenly went missing (in the future where courses are assignable. For now I'm just concerned
-  about a single enrollment and a single course, this is all future-proofing) there should be a way
-  to discover them! Either that addition will be "here's everyone, even the inactive students"
-  or "here's just the inactive students".
-*/
+const {
+  checkAuth,
+} = require("../../utils");
+
+const {
+  GraphQlDumpWarning,
+} = require("../../errors");
+
+const {
+  USER_TYPE_MODERATOR,
+  USER_STATUS_NORMAL,
+} = require("../../constants");
+
+const classroom = {
+  /**
+   * Get a classroom by a single classroomid. For moderators or better only. Students and Teachers
+   * can get it through me().
+   * @param parent
+   * @param args
+   *        classroomid: ID!
+   * @param ctx
+   * @param info
+   * @returns Classroom
+   */
+  async classroom(parent, args, ctx, info) {
+    // Must be moderator or better and normal.
+    await checkAuth(
+      ctx,
+      {
+        type: USER_TYPE_MODERATOR,
+        status: USER_STATUS_NORMAL,
+        action: "classroom",
+      },
+    );
+
+    if (!args.classroomid) {
+      throw new GraphQlDumpWarning("query", "classroom");
+    }
+
+    return ctx.db.query.classroom({ where: { id: args.classroomid } }, info);
+  },
+
+
+  /**
+   * Get a list of Classrooms by their IDs. For moderators or better only. Students and Teachers
+   * can get them through me().
+   * @param parent
+   * @param args
+   *        classroomids: [ID!]!
+   * @param ctx
+   * @param info
+   * @returns [Classroom]!
+   */
+  async classrooms(parent, args, ctx, info) {
+    // Must be moderator or better and normal.
+    await checkAuth(
+      ctx,
+      {
+        type: USER_TYPE_MODERATOR,
+        status: USER_STATUS_NORMAL,
+        action: "classrooms",
+      },
+    );
+
+    if (!Array.isArray(args.classroomids) || args.classroomids.length < 1) {
+      throw new GraphQlDumpWarning("query", "classrooms");
+    }
+
+    const queryClause = {
+      where: {
+        OR: args.classroomids.map(classroomId => ({ id: classroomId })),
+      },
+    };
+
+    return ctx.db.query.classrooms(queryClause, info);
+  },
+
+
+  /**
+   * Get a list of Classrooms by Prisma query search parameters. For Moderators or better only.
+   * Students and Teachers can get them through me().
+   * @param parent
+   * @param args
+   * @param ctx
+   * @param info
+   * @returns [Classroom]!
+   */
+  async classroomSearch(parent, args, ctx, info) {
+    // Must be moderator or better and normal.
+    await checkAuth(
+      ctx,
+      {
+        type: USER_TYPE_MODERATOR,
+        status: USER_STATUS_NORMAL,
+        action: "classroomSearch",
+      },
+    );
+
+    return ctx.db.query.subjects(args, info);
+  },
+};
+
+module.export = { classroom };
--- server/src/resolvers/Query/index.js
+++ server/src/resolvers/Query/index.js
@@ -1,11 +1,13 @@
 const { user } = require("./user");
 const { qa } = require("./qa");
 const { subject } = require("./subject");
+const { classroom } = require("./classroom");
 
 module.exports = {
   Query: {
     ...user,
     ...qa,
     ...subject,
+    ...classroom,
   },
 };
