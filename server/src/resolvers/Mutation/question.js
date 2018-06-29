e78f546056cffd009466aa3a4707ecf2da202a25
Kyle Geib <kyle.geib@gmail.com>
2018-06-28 22:49:27 -0700

    ISSUE-040 Server: Wrote dry-run checkSubmitQuestion mutation.
    Okay, this should perhaps be a query instead but whatever.
    
    Moved most of submitQuestion() into helper function checkAndParseInputs() where it can be all re-used. Nice!

--- server/src/resolvers/Mutation/question.js
+++ server/src/resolvers/Mutation/question.js
@@ -76,34 +76,9 @@ const question = {
       action: "submitQuestion",
     });
 
-    // Make sure that question input doesn't have multiple things in it.
-    if (args.questioninput &&
-      args.questioninput.conversioninput &&
-      args.questioninput.surveyrangeinput) {
-      throw new QuestionGenericError("Cannot have both conversioninput and surveyrangeinput defined");
-    }
-
-    // Create the question syntax string.
-    const questionSyntaxString = questionSyntaxFormatter(
-      args.questioninput.text,
-      args.questioninput.conversioninput || args.questioninput.surveyrangeinput,
-    );
-
-    // Create the answer syntax string.
-    const answerSyntaxString = answerSyntaxFormatter(
-      args.answerinput.string,
-      args.answerinput.multiplechoiceinput,
-      args.answerinput.conversioninput,
-    );
-
-    // Feed the inputted type, the question text, and the answer text into the parser.
-    // If there are any problems that were not caught in the syntax formatters above will
-    // throw errors now.
-    parseQAStrings(
-      args.type,
-      questionSyntaxString,
-      answerSyntaxString,
-    );
+    // Process the input to make the syntax strings and check for any mistakes.
+    const { questionSyntaxString, answerSyntaxString } =
+      checkAndParseInputs(args.type, args.questioninput, args.answerinput);
 
     // Create the question.
     return ctx.db.mutation.createQuestion({
@@ -128,6 +103,105 @@ const question = {
       },
     }, info);
   },
+
+
+  /**
+   * Provide a quick dry-run of submitQuestion. It'll throw up a bunch of errors if something is
+   * wronge, otherwise it'll return true. If this returns true, the question will be accepted
+   * TODO - This should probably be a Query instead of a Mutation... if you care, that is...
+   * @param parent
+   * @param args
+   *        type: Int!
+   *        questioninput: QuestionQuestionInput {
+   *          text: String
+   *            conversioninput: ConversionQuestionInput: {
+   *              lower: Float!
+   *              upper: Float!
+   *              unit: String!
+   *              step: Float
+   *            }
+   *            surveyrangeinput: RangeQuestionInput: {
+   *              lower: Float!
+   *              upper: Float!
+   *              unit: String!
+   *              step: Float
+   *            }
+   *        }!
+   *        answerinput: QuestionAnswerInput: {
+   *          text: String
+   *          multiplechoiceinput: MultipleChoiceInput: {
+   *            choices: [
+   *              MultipleChoiceInputRow: {
+   *                value: Float
+   *                written: String
+   *                unit: String!
+   *              }!
+   *            ]!
+   *            choicesoffered: Int
+   *          }
+   *          conversioninput: ConversionAnswerInput: {
+   *            unit: String!
+   *            accuracy: Float
+   *          }
+   *        }!
+   * @param ctx
+   * @returns {boolean}
+   */
+  async checkSubmitQuestion(parent, args, ctx) {
+    await checkAuth(ctx, {
+      type: USER_TYPE_STUDENT,
+      status: USER_STATUS_NORMAL,
+      action: "checkSubmitQuestion",
+    });
+
+    // Throws the input through the gauntlet. Anything wrong? Errors!
+    checkAndParseInputs(args.type, args.questioninput, args.answerinput);
+
+    // If it reaches this point the inputs were acceptable. Return true.
+    return true;
+  },
 };
+
+
+/**
+ * Helper function takes in the question's type, questioninput, and answerinput from the mutation
+ * inputs and does a ton of formatting and finally QA parse checking for acceptance.
+ * @param type
+ * @param questionInput
+ * @param answerInput
+ * @returns {{questionSyntaxString: string, answerSyntaxString: string}}
+ */
+function checkAndParseInputs(type, questionInput, answerInput) {
+  // Make sure that question input doesn't have multiple things in it.
+  if (questionInput &&
+    questionInput.conversioninput &&
+    questionInput.surveyrangeinput) {
+    throw new QuestionGenericError("Cannot have both conversioninput and surveyrangeinput defined");
+  }
+
+  // Create the question syntax string.
+  const questionSyntaxString = questionSyntaxFormatter(
+    questionInput.text,
+    questionInput.conversioninput || questionInput.surveyrangeinput,
+  );
+
+  // Create the answer syntax string.
+  const answerSyntaxString = answerSyntaxFormatter(
+    answerInput.string,
+    answerInput.multiplechoiceinput,
+    answerInput.conversioninput,
+  );
+
+  // Feed the inputted type, the question text, and the answer text into the parser.
+  // If there are any problems that were not caught in the syntax formatters above will
+  // throw errors now.
+  parseQAStrings(
+    type,
+    questionSyntaxString,
+    answerSyntaxString,
+  );
+
+  return { questionSyntaxString, answerSyntaxString };
+}
 
 module.exports = { question };
