258e76ee0f1b74d55b9835c39c005a20fbdb0c30
Kyle Geib <kyle.geib@gmail.com>
2018-07-07 12:36:08 -0700

    ISSUE-078 Server: Switched checkAuth() to use JWT datapayload, significantly reducing hits to the database.
    Dropped getUserId() from utils, replaced with getAuthTokenData().
    
    Fixed AuthPayload to share PrivateUser type instead of the limited User type.
    
    Adjusted login() to use args like all my other queries and mutations.

--- server/src/resolvers/Mutation/auth.js
+++ server/src/resolvers/Mutation/auth.js
@@ -33,7 +33,12 @@ const auth = {
     });
 
     return {
-      token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
+      token: jwt.sign({
+        id: user.id,
+        type: user.type,
+        status: user.status,
+        flags: user.flags,
+      }, process.env.APP_SECRET),
       user,
     };
   },
@@ -43,25 +48,31 @@ const auth = {
    * Log in to the account of a user by providing the email and password. Bcrypt is used
    * to check the password input for correctness.
    * @param parent
-   * @param email
-   * @param password
+   * @param args
+   *        email: String!
+   *        password: String!
    * @param ctx
    * @returns AuthPayload!
    */
-  async login(parent, { email, password }, ctx) {
-    const user = await ctx.db.query.user({ where: { email } });
+  async login(parent, args, ctx) {
+    const user = await ctx.db.query.user({ where: { email: args.email } });
     if (!user) {
-      throw new Error(`No such user found for email: ${email}`);
+      throw new Error(`No such user found for email: ${args.email}`);
     }
 
-    const valid = await bcrypt.compare(password, user.password);
+    const valid = await bcrypt.compare(args.password, user.password);
     if (!valid) {
       throw new Error("Invalid password");
     }
 
     // TODO Add login logging to database
     return {
-      token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
+      token: jwt.sign({
+        id: user.id,
+        type: user.type,
+        status: user.status,
+        flags: user.flags,
+      }, process.env.APP_SECRET),
       user,
     };
   },
