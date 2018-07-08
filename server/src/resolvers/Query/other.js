a194adcd33a54eae78ba6baf53a0e45ff1839995
Kyle Geib <kyle.geib@gmail.com>
2018-07-07 22:51:30 -0700

    ISSUE-082 Client: Remove Ping component and query from client.
    Server: Not going to delete the Ping query just yet... Could be useful in the future.
    Better error handling when the JWT is messed up (happens when first visiting the website).

--- server/src/resolvers/Query/other.js
+++ server/src/resolvers/Query/other.js
@@ -1,24 +1,14 @@
-const {
-  QUESTION_STATUS_ACTIVE,
-} = require("../../constants");
-
 const other = {
   /**
-   * Simple query that returns a string containing the date (to show freshness) and the number of
-   * active questions in the database (to show actual database information) without requiring any
-   * authorization as a way to provide an easy API access point for very early testing purposes.
-   * TODO - Remove this when the client is working.
+   * Simple query that returns a string containing the date to show the server is alive.
    * @param parent
    * @param args
    * @param ctx
    * @returns String!
    */
   async ping(parent, args, ctx) {
-    const questions =
-      await ctx.db.query.questions({ where: { status: QUESTION_STATUS_ACTIVE } }, "{ id }");
-
     const now = new Date(Date.now());
-    return `Hello! It is ${now.toString()} and there are ${questions.length} questions!`;
+    return `Hello! Server time is: ${now.toString()}`;
   },
 };
 
