c91077e9bad8da4d18bc63b3c9ab5ba8aeefa6de
Kyle Geib <kyle.geib@gmail.com>
2018-07-13 16:32:04 -0700

    ISSUE-114 Client: Using the new-and-improved withAuth HOC to provide userTokenData to UserDetailEnrollment and UserDetailCourse.
    I use it to prevent users besides moderators and admins from being able to see the buttons to enroll and assign a course for a student. This wouldn't happen right now, but it's a future-proof against, I dunno, when I write the ability for teachers to view their students' pages.
    
    With this feature all I need to do is add a withAuth wrapper and, boom, instant access to userTokenData which will allow me to grab all sorts of useful things.
    
    Also added a new fast-start script for the time you know what you're doing any want to skip the SemanticUI CSS build process.

--- client/src/components/user/details/UserDetailEnrollment.js
+++ client/src/components/user/details/UserDetailEnrollment.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import withAuth from "../../AuthHOC";
+
 import UserDetailCourse from "./UserDetailCourse";
 import UserDetailEnroll from "./UserDetailEnroll";
 
@@ -26,10 +28,13 @@ const UserDetailEnrollment = (props) => {
     return (
       <div>
         <p>Not enrolled!</p>
-        <UserDetailEnroll
-          studentId={user.id}
-          userQuery={props.userQuery}
-        />
+        {/* In addition to the student, allow moderators or better to Enroll the student */}
+        {(user.id === props.userTokenData.id || props.userTokenData.type >= 2) &&
+          <UserDetailEnroll
+            studentId={user.id}
+            userQuery={props.userQuery}
+          />
+        }
       </div>
     );
   } else {
@@ -51,10 +56,14 @@ UserDetailEnrollment.propTypes = {
       }), // Not required.
     }).isRequired,
   }),
+  userTokenData: PropTypes.shape({
+    type: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 UserDetailEnrollment.defaultProps = {
   queryData: null,
 };
 
-export default UserDetailEnrollment;
+export default withAuth(UserDetailEnrollment);  // provide access to userTokenData
