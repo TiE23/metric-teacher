5bedeab193eaeb69601aef22d3ca625bb098b7a5
Kyle Geib <kyle.geib@gmail.com>
2018-07-12 18:01:54 -0700

    ISSUE-089 Client: New UserDetailMasteries and UserDetailMastery components.

--- client/src/components/user/details/UserDetailCourse.js
+++ client/src/components/user/details/UserDetailCourse.js
@@ -1,12 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import UserDetailMasteries from "./UserDetailMasteries";
+
 const UserDetailCourse = (props) => {
   if (props.courses.length) {
     const course = props.courses[0];
     return (
       <div>
         <p>Course ID: {course.id}</p>
+        <UserDetailMasteries masteries={course.masteries} />
       </div>
     );
   } else {
@@ -21,6 +24,7 @@ const UserDetailCourse = (props) => {
 UserDetailCourse.propTypes = {
   courses: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.string.isRequired,
+    masteries: PropTypes.array,
   })).isRequired,
 };
 
