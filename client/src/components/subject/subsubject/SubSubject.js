1c0f2ecf30af14e673150e7faff943620fa10151
Kyle Geib <kyle.geib@gmail.com>
2018-07-17 23:08:42 -0700

    ISSUE-108 Client: Updated the basic SubSubject component to show Mastery data.
    I'm a bit weirded out by how I'm going to display this whole system in reverse based on a list of Masteries for the UserDetails section of the website. But I'll get to it when I get to it.

--- client/src/components/subject/subsubject/SubSubject.js
+++ client/src/components/subject/subsubject/SubSubject.js
@@ -3,9 +3,16 @@ import PropTypes from "prop-types";
 
 const SubSubject = (props) => {
   const { subSubjectData } = props;
+  const mastery = subSubjectData.masteries && subSubjectData.masteries.length !== 0 ?
+    subSubjectData.masteries[0] : null;
 
   return (
-    <div>--SubSubject: {subSubjectData.name} ({subSubjectData.id})</div>
+    <div>
+      --SubSubject: {subSubjectData.name} ({subSubjectData.id})
+      {mastery &&
+        <span>----Mastery - Status: {mastery.status} - Score: {mastery.score} </span>
+      }
+    </div>
   );
 };
 
@@ -13,6 +20,10 @@ SubSubject.propTypes = {
   subSubjectData: PropTypes.shape({
     id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
+    masteries: PropTypes.arrayOf(PropTypes.shape({
+      status: PropTypes.number.isRequired,
+      score: PropTypes.number.isRequired,
+    })),
   }).isRequired,
   publicList: PropTypes.bool,
 };
