92e74656c3cf4fb9eb3d0b69d50efbce8a1cc472
Kyle Geib <kyle.geib@gmail.com>
2018-07-27 20:26:28 -0700

    ISSUE-139 Server: Query generateChallenge now uses a student ID instead of a Course ID to work.

--- server/src/resolvers/Query/qa.js
+++ server/src/resolvers/Query/qa.js
@@ -7,7 +7,7 @@ const {
 
 const {
   AuthError,
-  CourseNotFound,
+  StudentNoActiveCourse,
   CourseInactive,
   QuestionNotFound,
 } = require("../../errors");
@@ -27,6 +27,7 @@ const {
   ChallengeGenerator,
 } = require("../../logic/challengeGenerator");
 
+const queryCourse = require("../Query/course");
 
 const qa = {
   /**
@@ -138,11 +139,11 @@ const qa = {
 
 
   /**
-   * Generate an entire list of QAObjects (called a "Challenge") based off a Course ID and
+   * Generate an entire list of QAObjects (called a "Challenge") based off a student ID and
    * a list of Subject or SubSubject IDs. Additional boolean arguments help customize the results.
    * @param parent
    * @param args
-   *        courseid: ID!
+   *        studentid: ID!
    *        subjectids: [ID]
    *        subsubjectids: [ID]
    *        listsize: Int!
@@ -154,47 +155,37 @@ const qa = {
    */
   async generateChallenge(parent, args, ctx) {
     // Must be normal status.
-    const callingUserData = await checkAuth(ctx, {
+    await checkAuth(ctx, {
       type: USER_TYPE_STUDENT,
       status: USER_STATUS_NORMAL,
       action: "generateChallenge",
     });
 
-    const courseData = await ctx.db.query.course(
-      {
-        where: { id: args.courseid },
-      },
-      `{
-        status
-        parent {
-          student {
-            id
+    const targetCourseData =
+      await queryCourse.course.activeCourse(parent, { studentid: args.studentid }, ctx, `
+        {
+          id
+          status
+          parent {
+            student {
+              id
+            }
           }
         }
-      }`,
-    );
+      `);
 
     // If the Course cannot be found, stop here.
-    if (!courseData) {
-      throw new CourseNotFound(args.courseid);
+    if (!targetCourseData) {
+      throw new StudentNoActiveCourse(args.studentid, "generateChallenge");
     }
 
-    // If calling user is a Student check that the Course belongs to the student.
-    if (callingUserData.type === USER_TYPE_STUDENT &&
-      callingUserData.id !== courseData.parent.student.id) {
-      throw new AuthError(
-        `Student ${callingUserData.id} cannot generate a Challenge for a Course (${args.courseid}) that doesn't belong to them`,
-        "generateChallenge",
-      );
-    }
-
-    if (courseData.status !== COURSE_STATUS_ACTIVE) {
-      throw new CourseInactive(args.courseid);
+    if (targetCourseData.status !== COURSE_STATUS_ACTIVE) {
+      throw new CourseInactive(targetCourseData.id);
     }
 
     const ChallengeGen = new ChallengeGenerator(ctx);
     return ChallengeGen.generateChallenge(
-      args.courseid,
+      targetCourseData.id,
       args.subjectids || [],
       args.subsubjectids || [],
       args.listsize,
