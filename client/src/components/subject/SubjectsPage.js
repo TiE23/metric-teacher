99dd09a9744d4412971521a755567bed6db1908f
Kyle Geib <kyle.geib@gmail.com>
2018-08-06 18:41:26 -0700

    ISSUE-144 Client: Subjects page looking a lot better.
    It has different written details for logged-out and logged-in users. It now offers buttons to login/signup. This required hooking it up to the router HOC, so I decided to put it into its own child component SubjectsPageDescription.

--- client/src/components/subject/SubjectsPage.js
+++ client/src/components/subject/SubjectsPage.js
@@ -6,18 +6,22 @@ import { Grid } from "semantic-ui-react";
 import Subjects from "./Subjects";
 
 import {
+  FLOATING_CENTER_GRID_COLUMN_WIDTH_WIDE,
   USER_TYPE_STUDENT,
 } from "../../constants";
 
 const SubjectsPage = props => (
   <Grid padded>
-    <Grid.Row>
-      <Grid.Column>
-        <p>Subjects</p>
+    <Grid.Row centered>
+      <Grid.Column {...FLOATING_CENTER_GRID_COLUMN_WIDTH_WIDE}>
         <Subjects
           studentId={props.userTokenData && props.userTokenData.type === USER_TYPE_STUDENT ?
             props.userTokenData.id : null}
         />
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column {...FLOATING_CENTER_GRID_COLUMN_WIDTH_WIDE}>
         <button onClick={props.history.goBack}>Go back</button>
       </Grid.Column>
     </Grid.Row>
