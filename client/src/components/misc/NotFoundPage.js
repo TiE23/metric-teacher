15a6e9438918465fd5f892e8062322396d2ad5c2
Kyle Geib <kyle.geib@gmail.com>
2018-11-16 16:45:29 -0800

    ISSUE-259 Client: Added mascot to 404 page (NotFoundPage).
    Removed the shoddy routing stuff as it was unreliable. Replaced with a single button.

--- client/src/components/misc/NotFoundPage.js
+++ client/src/components/misc/NotFoundPage.js
@@ -1,37 +1,39 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { withRouter } from "react-router";
-import { Button, Grid, Header, Image, Segment } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Button, Grid, Header, Icon, Image, Segment } from "semantic-ui-react";
 
 import FloatingCenterGrid from "../FloatingCenterGrid";
 
-const NotFoundPage = props => (
+import {
+  PAGE_TITLE_HEADER_SIZE,
+} from "../../constants";
+
+const NotFoundPage = () => (
   <FloatingCenterGrid>
     <Segment raised>
       <Grid stackable columns={2}>
         <Grid.Row>
           <Grid.Column width={16}>
-            <Header size="large" textAlign="center">
-              Page Not Found
+            <Header size={PAGE_TITLE_HEADER_SIZE} textAlign="center">
+              <Header.Content>
+                <Icon name="warning sign" color="orange" />
+                404
+                <Header.Subheader>
+                  Page Not Found
+                </Header.Subheader>
+              </Header.Content>
             </Header>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
           <Grid.Column>
-            <Image src="/img/placeholder.png" />
+            <Image src="/img/mascot/404.gif" size="medium" centered circular />
           </Grid.Column>
           <Grid.Column textAlign="center">
             <p>
-              In the Internet biz we call this a <b>404 Error</b>.
+              Hopefully you weren&apos;t sent here by mistake!
             </p>
-            <p>
-              Hopefully we didn&apos;t send you here by mistake!
-            </p>
-            {props.history.length > 2 ?
-              <Button onClick={props.history.goBack}>Go back</Button>
-              :
-              <Button onClick={() => props.history.push("/")}>Go to main page</Button>
-            }
+            <Button as={Link} to="/" primary>Go to main page</Button>
           </Grid.Column>
         </Grid.Row>
       </Grid>
@@ -39,12 +41,4 @@ const NotFoundPage = props => (
   </FloatingCenterGrid>
 );
 
-NotFoundPage.propTypes = {
-  history: PropTypes.shape({
-    length: PropTypes.number.isRequired,
-    goBack: PropTypes.func.isRequired,
-    push: PropTypes.func.isRequired,
-  }).isRequired,
-};
-
-export default withRouter(NotFoundPage);
+export default NotFoundPage;
