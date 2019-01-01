15733967c9a9a67710814dd641a274c6f7db6d12
Kyle Geib <kyle.geib@gmail.com>
2018-12-31 17:44:46 -0800

    ISSUE-331 Client: Adding Inspiration section of Demo page.
    Also added styling to multiple choice component to make the mouse cursor into a pointer. It's nice this way.

--- client/src/components/misc/demo/DemoPage.js
+++ client/src/components/misc/demo/DemoPage.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Container, Header, Icon, Image } from "semantic-ui-react";
 
 import ScrollToTopOnMount from "../ScrollToTopOnMount";
-import ExternalLink from "../ExternalLink";
+import XLink from "../ExternalLink";
 
 import {
   PAGE_ICON_COLOR_CREDITS,
@@ -19,14 +19,14 @@ const DemoPage = () => (
       {SITE_NAME} Demo
     </Header>
 
-    <Image src="/img/credits/kyle.gif" size="small" floated="right" bordered rounded />
-    <Header as="h2" content="Personal Introduction" />
+    <Header as="h2" content="Personal Introduction" dividing />
+    <Image src="/img/credits/kyle.gif" size="small" floated="right" rounded />
 
     <p>
       Hello! I'm <b>Kyle Geib</b> and I'm a Seattle-based developer who graduated from <i>University of Washington Bothell</i> and worked for five years at <i>Accretive Technology Group</i> (ATG), an independent web company that specializes in video streaming.
     </p>
 
-    <Header as="h3" content="My time at ATG" />
+    <Header as="h3" content="My Time at ATG" />
 
     <p>
       In 2013 I started as an <b>intern</b> and became one of their first <b>SDET</b>s. In 2015 I became a <b>Support Engineer</b>, essentially a programming handyman. Fixing hideous bugs in PHP, writing automation scripts, designing CLI tools in Python, writing crazy MySQL queries, and building basic admin tools using jQuery.
@@ -45,7 +45,33 @@ const DemoPage = () => (
     </p>
 
     <p>
-      By May I had pushed my <ExternalLink to="https://github.com/TiE23/metric-teacher">first commit</ExternalLink> to {SITE_NAME} and after <b>eight months</b> of full-time, solo work I finally brought my first website live on <b>December 20th, 2018</b>.
+      By May I had pushed my <XLink to="https://github.com/TiE23/metric-teacher">first commit</XLink> to {SITE_NAME} and after <b>eight months</b> of full-time, solo work I finally brought my first website live on December 20th, 2018.
+    </p>
+
+    <Header as="h2" content={`The Inspiration Behind ${SITE_NAME}`} dividing />
+
+    <p>
+      A few years ago I was using <XLink to="https://www.duolingo.com/"><b>Duolingo</b></XLink>, a site that teaches foreign languages through repetition of <b>programmatically generated flash-cards</b> that you return to for 10 minutes a day. I thought that the same method could work for teaching Americans the Metric System.
+    </p>
+
+    <Header as="h3" content="Thinking In Metric" />
+
+    <p>
+      I've had experience driving rental cars in Ireland, France, and Japan where I found myself becoming comfortable with kilometers per hour. After some time <i>I wasn't thinking</i> "Oh, 50 km/h is about 30 mph", I was simply <b>thinking</b> in kilometers per hour.
+    </p>
+
+    <p>
+      If I could teach people that 50 km/h was equivalent to what they thought about 30 mph ("that's city-street speeds" or "about the fastest I could ride a bicycle"), then that'd be the first step.
+    </p>
+
+    <Header as="h3" content="Making It Personal" />
+
+    <p>
+      How <b>tall</b> are you? How much do you <b>weigh</b>? What's the coolest <b>outdoor temperature</b> that you'd wear shorts in?  We all have answers to these questions even if they might be hard to pin down. But if you can learn to recognize that 21°C and 70°F are both your preferred room temperature then you'll start to simply feel in Celsius.
+    </p>
+
+    <p>
+      This idea is {SITE_NAME}'s <b>Survey Question</b> system. By asking people meaningful questions they can begin associating Metric unit measurements with those answers.
     </p>
   </Container>
 );
