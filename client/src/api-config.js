f414fe27609b84d54a928321a9faa67ec0978a3b
Kyle Geib <kyle.geib@gmail.com>
2019-01-07 19:49:57 -0800

    ISSUE-341 Client: Basic Traefik configuration working in test.
    No HTTPS redirect or anything going on yet, but the client is appearing, so I'm happy.
    
    Dropped the metricteacher.app idea for now.

--- client/src/api-config.js
+++ client/src/api-config.js
@@ -8,16 +8,14 @@ let API_ROOT;
 // Figure out the full host root.
 if (process.env.METRIC_TEACHER_SERVER_BACKEND_HOST) {
   API_ROOT = `${process.env.METRIC_TEACHER_SERVER_BACKEND_HOST}:${port}`;
-} else if (/amazonaws.com$/.test(hostname)) {       // AWS testing
+} else if (/amazonaws.com$/.test(hostname)) {           // AWS testing
   API_ROOT = `${hostname}:${port}`;
-} else if (/dev.metric-teacher.com$/.test(hostname)) {  // First domain name (dev)
+} else if (/dev.metric-teacher.com$/.test(hostname)) {  // Dev
   API_ROOT = `${hostname}:${port}`;
-} else if (/metric-teacher.com$/.test(hostname)) {      // First domain name (prod)
+} else if (/test.metric-teacher.com$/.test(hostname)) { // HTTPS testing
   API_ROOT = "api.metric-teacher.com";
-} else if (/dev.metricteacher.app$/.test(hostname)) {   // Second domain name (dev)
-  API_ROOT = `${hostname}:${port}`;
-} else if (/metricteacher.app$/.test(hostname)) {       // Second domain name (prod)
-  API_ROOT = "api.metricteacher.app";
+} else if (/metric-teacher.com$/.test(hostname)) {      // Production
+  API_ROOT = "api.metric-teacher.com";
 } else if (/^192\.168\.1\./.test(hostname)) {           // Local network testing
   API_ROOT = `${hostname}:${port}`;
 } else if (/^10\.0\.0\./.test(hostname)) {              // Local network testing
