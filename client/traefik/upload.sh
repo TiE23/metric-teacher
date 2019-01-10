21f407133fea06d004a309ab0692741e566ba0fd
Kyle Geib <kyle.geib@gmail.com>
2019-01-09 18:31:39 -0800

    ISSUE-341 Client: Updating scripts.

--- client/traefik/upload.sh
+++ client/traefik/upload.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
-if [[ ! -f "acme.json" ]]; then
-  echo "acme.json doesn't exist!";
-  exit 1;
-fi
-
 if [[ $# -eq 0 ]]; then
   echo "Run with destination domain name as argument";
   exit 1;
 fi
 
-scp -p acme.json traefik.toml ubuntu@${1}:~/;
+if [[ ! -f "acme.json" ]]; then
+  echo "acme.json doesn't exist so we're not uploading it";
+else
+  echo "acme.json exists so we're uploading it";
+  scp -p acme.json ubuntu@${1}:~/;
+fi
+
+if [[ ! -f "traefik.toml" ]]; then
+  echo "traefik.toml doesn't exist so we're not uploading it";
+else
+  echo "traefik.toml exists so we're uploading it";
+  scp -p traefik.toml ubuntu@${1}:~/;
+fi
