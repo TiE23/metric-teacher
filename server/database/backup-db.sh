b459ad9623d2a402cacf225083d5b5208f2d2f21
Kyle Geib <kyle.geib@gmail.com>
2019-01-10 23:36:44 -0800

    ISSUE-341 Server: Improving backup-db.sh script.
    It stopped working for some reason. Now it should work again.

--- server/database/backup-db.sh
+++ server/database/backup-db.sh
@@ -11,29 +11,45 @@ if [[ $# -eq 0 ]]; then
   exit 1
 fi
 
-# Yes, raw password because a hacker would need to get past firewall and layers of auth to even get to this point.
-ssh ubuntu@$1 sudo docker exec database_db_1 bash -c "mysqldump --default-character-set=utf8mb4 -u root --password=prisma --all-databases > backup.sql"
+DATE_STAMP=`date -u +%Y-%m-%d_%H-%M-%S`
+BACKUP_TEMP_NAME=backup_${DATE_STAMP}.sql
 
-DB_CONTAINER_ID=`ssh ubuntu@$1 sudo docker ps -q -f "name=database_db_1"`
+# Yes, raw password because a hacker would need to get past firewall and layers of auth to even get to this point.
+ssh ubuntu@$1 "sudo docker exec database_db_1 bash -c \"mysqldump --default-character-set=utf8mb4 -u root --password=prisma --all-databases > ${BACKUP_TEMP_NAME}\""
+
+DB_CONTAINER_ID=`ssh ubuntu@$1 "sudo docker ps -q -f \"name=database_db_1\""`
 
 # Copy backup from container to host
-ssh ubuntu@$1 sudo docker cp $DB_CONTAINER_ID:backup.sql backup.sql
+ssh ubuntu@$1 "sudo docker cp ${DB_CONTAINER_ID}:${BACKUP_TEMP_NAME} ${BACKUP_TEMP_NAME}"
+
+# Delete backup from container
+ssh ubuntu@$1 "sudo docker exec database_db_1 bash -c \"rm ${BACKUP_TEMP_NAME}\""
 
 # Using UTC Timestamp
 # By the way, the directory backups/ is a symlink to a good backup location for me.
 if [[ -z $2 ]]; then
-  BACKUP_LOCATION=backups/db_$1_`date -u +%Y-%m-%d_%H-%M-%S`_manual.sql
+  BACKUP_LOCATION=backups/db_$1_${DATE_STAMP}_manual.sql
 else
   # Second arg can give it an extra suffix. For example, in cron I have it say "..._cron.sql"
-  BACKUP_LOCATION=backups/db_$1_`date -u +%Y-%m-%d_%H-%M-%S`_$2.sql
+  BACKUP_LOCATION=backups/db_$1_${DATE_STAMP}_$2.sql
 fi
 
-scp ubuntu@$1:backup.sql $BACKUP_LOCATION
+scp ubuntu@$1:${BACKUP_TEMP_NAME} ${BACKUP_LOCATION} && ssh ubuntu@$1 "rm ${BACKUP_TEMP_NAME}"
 
-echo "Backup complete: $BACKUP_LOCATION"
+echo "Backup complete: ${BACKUP_LOCATION} - top line:"
+head -n 1 ${BACKUP_LOCATION}
 
 # I'm not going to write a script for restoring because I don't ever want to do it by accident.
-# The command to copy the file over
-#   docker cp backup-file.sql <CONTAINER_ID>:backup-file.sql
+# So here is a guide:
+# To copy the file to the host:
+#   scp backup-file.sql ubuntu@db.metric-teacher.com:backup-file.sql
+# Then, of course, SSH into the host:
+#   ssh ubuntu@db.metric-teacher.com
+# The command to see the containers:
+#   sudo docker ps
+# The command to copy the file over:
+#   sudo docker cp backup-file.sql <CONTAINER_ID>:backup-file.sql
+# Go into the container itself (name might be different):
+#   sudo docker exec -it database_db_1 bash
 # The command to restore it after bashing into the container:
 #   mysql --default-character-set=utf8mb4 -u root --password=prisma < backup-file.sql
