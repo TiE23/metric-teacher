b115e87f5a173218142805896fe9b2798a03c240
Kyle Geib <kyle.geib@gmail.com>
2019-01-14 12:44:02 -0800

    ISSUE-330 Server: Fixed bugged permission checking in checkAuth().
    Updated docker-compose files to turn off verbose logging with PRISMA_DEBUG=false.
    
    In retrospect this would've exposed people's passwords to logging... well, thankfully those logs weren't saved anywhere... but then again, _I_ could've seen them. Well, now I won't anymore...
    
    Client: Same bug fix for checkAuth().
    
    Can't believe I never caught this, haha.

--- server/src/utils.js
+++ server/src/utils.js
@@ -185,12 +185,12 @@ async function checkAuth(
   if (Array.isArray(permissions.type)) {
     if (!permissions.type.includes(callingUserData.type)) {
       approval = false;
-      rejectionReasons.push(`User type '${callingUserData.type}' disallowed.`);
+      rejectionReasons.push(`User type '${callingUserData.type}' not allowed.`);
     }
   } else if (Number.isInteger(permissions.type)) {
     if (callingUserData.type < permissions.type) {
       approval = false;
-      rejectionReasons.push(`User type '${callingUserData.type}' insufficient.`);
+      rejectionReasons.push(`User type '${callingUserData.type}' not allowed.`);
     }
   }
 
@@ -198,12 +198,12 @@ async function checkAuth(
   if (Array.isArray(permissions.status)) {
     if (permissions.status.indexOf(callingUserData.status) === -1) {
       approval = false;
-      rejectionReasons.push(`User status '${callingUserData.status}' disallowed.`);
+      rejectionReasons.push(`User status '${callingUserData.status}' not allowed.`);
     }
   } else if (Number.isInteger(permissions.status)) {
-    if (callingUserData.status < permissions.status) {
+    if (callingUserData.status > permissions.status) {
       approval = false;
-      rejectionReasons.push(`User status '${callingUserData.status}' insufficient.`);
+      rejectionReasons.push(`User status '${callingUserData.status}' not allowed.`);
     }
   }
 
